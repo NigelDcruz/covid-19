a534a132ca28ec1885abda5330a8639b1caabb90
Siona Fernandes <sionac.fernandes@gmail.com>
2020-03-29 01:27:43 +0530

    fixes:covid-chart

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let allCaseNo = document.getElementById("allCaseNo");
 let recoveredCaseNo = document.getElementById("recoveredCaseNo");
 let deathCaseNo = document.getElementById("deathCaseNo");
 
-// let doughnutChart = document.getElementById("doughnutChart").getContext("2d");
+let covidChart = document.getElementById("covidCasesChart").getContext("2d");
 
 (async () => {
 	let data = await fetch("https://corona.lmao.ninja/all");
@@ -19,36 +19,39 @@ let deathCaseNo = document.getElementById("deathCaseNo");
 	recoveredCaseNo.innerText = allRecovered;
 	deathCaseNo.innerText = allDeaths;
 
-	// let donut = new Chart(doughnutChart, {
-	// 	// The type of chart we want to create
-	// 	type: "doughnut",
+	//Covid Chart
+	let donut = new Chart(covidChart, {
+		// The type of chart we want to create
+		type: "doughnut",
 
-	// 	// The data for our dataset
-	// 	data: {
-	// 		labels: ["All Cases", "Recovered Cases", "Death Cases"],
-	// 		datasets: [
-	// 			{
-	// 				label: "Covid-19 Cases",
-	// 				borderColor: "rgba(0,0,0)",
-	// 				backgroundColor: [
-	// 					"rgba(136,136,136,1)",
-	// 					"rgba(0,255,0,1)",
-	// 					"rgba(255,0,0,1)"
-	// 				],
-	// 				data: [allCases, allRecovered, allDeaths]
-	// 			}
-	// 		]
-	// 	},
-	// 	options: {
-	// 		legend: {
-	// 			labels: {
-	// 				fontSize: 25,
-	// 				padding: 50
-	// 			}
-	// 		},
-	// 		tooltips: {
-	// 			bodyFontSize: 30
-	// 		}
-	// 	}
-	// });
+		// The data for our dataset
+		data: {
+			labels: ["All Cases", "Recovered Cases", "Death Cases"],
+			datasets: [
+				{
+					label: "Covid-19 Cases",
+					borderColor: "rgba(0,0,0)",
+					backgroundColor: [
+						"rgba(136,136,136,1)",
+						"rgba(0,255,0,1)",
+						"rgba(255,0,0,1)"
+					],
+					data: [allCases, allRecovered, allDeaths]
+				}
+			]
+		},
+		options: {
+			//changing legends styles
+			legend: {
+				labels: {
+					fontSize: 25,
+					padding: 50
+				}
+			},
+			//tooltips styles
+			tooltips: {
+				bodyFontSize: 30
+			}
+		}
+	});
 })();
