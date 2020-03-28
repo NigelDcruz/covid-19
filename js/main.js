d6728bed07f89efe839d58003d16ec28a28c7c08
Siona Fernandes <sionac.fernandes@gmail.com>
2020-03-29 01:10:42 +0530

    doughnut chart added

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ let allCaseNo = document.getElementById("allCaseNo");
 let recoveredCaseNo = document.getElementById("recoveredCaseNo");
 let deathCaseNo = document.getElementById("deathCaseNo");
 
+// let doughnutChart = document.getElementById("doughnutChart").getContext("2d");
+
 (async () => {
 	let data = await fetch("https://corona.lmao.ninja/all");
 
@@ -16,4 +18,37 @@ let deathCaseNo = document.getElementById("deathCaseNo");
 	allCaseNo.innerText = allCases;
 	recoveredCaseNo.innerText = allRecovered;
 	deathCaseNo.innerText = allDeaths;
+
+	// let donut = new Chart(doughnutChart, {
+	// 	// The type of chart we want to create
+	// 	type: "doughnut",
+
+	// 	// The data for our dataset
+	// 	data: {
+	// 		labels: ["All Cases", "Recovered Cases", "Death Cases"],
+	// 		datasets: [
+	// 			{
+	// 				label: "Covid-19 Cases",
+	// 				borderColor: "rgba(0,0,0)",
+	// 				backgroundColor: [
+	// 					"rgba(136,136,136,1)",
+	// 					"rgba(0,255,0,1)",
+	// 					"rgba(255,0,0,1)"
+	// 				],
+	// 				data: [allCases, allRecovered, allDeaths]
+	// 			}
+	// 		]
+	// 	},
+	// 	options: {
+	// 		legend: {
+	// 			labels: {
+	// 				fontSize: 25,
+	// 				padding: 50
+	// 			}
+	// 		},
+	// 		tooltips: {
+	// 			bodyFontSize: 30
+	// 		}
+	// 	}
+	// });
 })();
