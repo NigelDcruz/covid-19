995b60007c14e87f7361ecce5e4abfb9ff4eb096
Siona Fernandes <sionac.fernandes@gmail.com>
2020-03-28 20:13:33 +0530

    cases added

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 // Gets All global cases
 (async () => {
-  let data = await fetch("https://corona.lmao.ninja/all");
+	let data = await fetch("https://corona.lmao.ninja/all");
 
-  let all = await data.json();
+	let all = await data.json();
 
-  let allCases = all.cases;
-  let allDeaths = all.deaths;
-  let allRecovered = all.recovered;
+	let allCases = all.cases;
+	let allDeaths = all.deaths;
+	let allRecovered = all.recovered;
 
-  console.log(allCases);
-  console.log(allDeaths);
-  console.log(allRecovered);
-  
+	document.getElementById("allCasesNo").innerHTML = allCases;
+	document.getElementById("recoveredCasesNo").innerHTML = allRecovered;
+	document.getElementById("deathCasesNo").innerHTML = allDeaths;
+
+	console.log(allCases);
+	console.log(allDeaths);
+	console.log(allRecovered);
 })();
