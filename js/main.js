b9d4e3540bc1cbd50187c86a89de24189d82ea70
Nigel Dcruz <nigeldcruz7@gmail.com>
2020-04-02 20:49:49 +0530

    Made styling improvements

--- js/main.js
+++ js/main.js
@@ -6,72 +6,81 @@ let deathCaseNo = document.getElementById("deathCaseNo");
 // Get the CovidChart canvas element
 let covidChart = document.getElementById("covidCasesChart").getContext("2d");
 
+
+// Helper Functions ================================================================
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
+
 // ==================================================================================
 
 //Replace Valuse
 let handleReplaceAllCasesValues = (all, recovered, death) => {
-	allCaseNo.innerText = all;
-	recoveredCaseNo.innerText = recovered;
-	deathCaseNo.innerText = death;
+  all = numberWithCommas(all)
+  recovered = numberWithCommas(recovered)
+  death = numberWithCommas(death)
+
+  allCaseNo.innerText = all;
+  recoveredCaseNo.innerText = recovered;
+  deathCaseNo.innerText = death;
 };
 
 // Renders All Cases
 let renderCovidChart = (all, recovered, death) => {
-	//Covid Chart
-	let donut = new Chart(covidChart, {
-		// The type of chart we want to create
-		type: "doughnut",
+  //Covid Chart
+  let donut = new Chart(covidChart, {
+    // The type of chart we want to create
+    type: "bar",
 
-		// The data for our dataset
-		data: {
-			labels: ["All Cases", "Recovered Cases", "Death Cases"],
-			datasets: [
-				{
-					label: "Covid-19 Cases",
-					borderColor: "rgba(0,0,0)",
-					backgroundColor: [
-						"rgba(136,136,136,1)",
-						"rgba(0,255,0,1)",
-						"rgba(255,0,0,1)"
-					],
-					data: [all, recovered, death]
-				}
-			]
-		},
-		options: {
-			responsive: true,
-			maintainAspectRatio: false,
+    // The data for our dataset
+    data: {
+      labels: ["All Cases", "Recovered Cases", "Death Cases"],
+      datasets: [{
+        label: "Covid-19 Cases",
+        borderColor: "rgba(0,0,0)",
+        backgroundColor: [
+          "#86c6be",
+          "#a6c64c",
+          "#c80003",
+        ],
+        data: [all, recovered, death]
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
 
-			//changing legends styles
-			legend: {
-				labels: {
-					fontSize: 25,
-					padding: 20
-				}
-			},
-			//tooltips styles
-			tooltips: {
-				bodyFontSize: 25
-			}
-		}
-	});
+      //changing legends styles
+      legend: {
+        labels: {
+          fontSize: 25,
+          padding: 20
+        }
+      },
+      //tooltips styles
+      tooltips: {
+        bodyFontSize: 25
+      }
+    }
+  });
 };
 
 // ==================================================================================
 
 // Fetch data for all covid-19 cases
 (async () => {
-	let data = await fetch("https://corona.lmao.ninja/all");
+  let data = await fetch("https://corona.lmao.ninja/all");
 
-	let all = await data.json();
+  let all = await data.json();
 
-	let allCases = all.cases;
-	let allDeaths = all.deaths;
-	let allRecovered = all.recovered;
+  let allCases = all.cases;
+  let allDeaths = all.deaths;
+  let allRecovered = all.recovered;
 
-	//Replace the dom values
-	handleReplaceAllCasesValues(allCases, allRecovered, allDeaths);
+  //Replace the dom values
+  handleReplaceAllCasesValues(allCases, allRecovered, allDeaths);
 
-	//Draw Chart
-	renderCovidChart(allCases, allRecovered, allDeaths);
+  //Draw Chart
+  renderCovidChart(allCases, allRecovered, allDeaths);
 })();
